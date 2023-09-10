94622ac65535d37e5433a8dcf40190f8f2fcfe30
Arne Breitsprecher <arnebr@gmail.com>
2023-09-10 09:29:56 +0000

    Adding bridge for ticket number and allow queryTicket to have kwid

--- index.js
+++ index.js
@@ -3,5 +3,27 @@
  * ISC Licensed
  */
 
-import queryTicket from './lib/queryTicket.js' 
-export default queryTicket
+import {default  as rqorderdetails} from './lib/queryTicket.js' 
+import findTicket from './lib/findTicket.js' 
+
+/**
+ * queryTicket function, returns the ticket data based on the ticket number and last name. handles both old and new ticket system
+ * @param   string | DB ticket number (ticket number up 12 digits or 6 characters)
+ * @param   lastName | last name of purchaser
+ * @return  Promise | fulfills with an object that contains the ticket data
+ */
+const queryTicket = async (ticketNumber, lastName) => {
+    //create a null kwid variable
+    let kwid = null
+    if (ticketNumber.length > 5) {
+        const order = await findTicket(ticketNumber, lastName) 
+        //extract the kwid from the order object
+        kwid = order.kwid
+    }
+    //use the queryTicket function to get the ticket data
+    const ticketData = await rqorderdetails(ticketNumber, lastName, kwid)
+    return ticketData
+}
+
+
+export default queryTicket 
